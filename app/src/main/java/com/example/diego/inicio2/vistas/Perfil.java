61834af4f4b56d13d2a386251c375379b84ce188
smatiassosa <sosa.sergiomatias@gmail.com>
2015-08-20 18:33:58 -0300

    Listo Layout Mi Perfil

--- app/src/main/java/com/example/diego/inicio2/vistas/Perfil.java
+++ app/src/main/java/com/example/diego/inicio2/vistas/Perfil.java
@@ -1,10 +1,16 @@
 package com.example.diego.inicio2.vistas;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.diego.inicio2.R;
 
@@ -16,6 +22,24 @@ public class Perfil extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.f_perfil, container, false);
+
+        Drawable originalDrawable = getResources().getDrawable(R.drawable.pipi);
+        Bitmap originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
+
+        originalBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, 1000, 1000);
+
+        //creamos el drawable redondeado
+        RoundedBitmapDrawable roundedDrawable =
+                RoundedBitmapDrawableFactory.create(getResources(), originalBitmap);
+
+        //asignamos el CornerRadius
+        roundedDrawable.setCornerRadius(originalBitmap.getWidth());
+
+        ImageView imageView = (ImageView)(rootView).findViewById(R.id.imgUsuario_MiPerfil);
+
+        imageView.setImageDrawable(roundedDrawable);
+
+
         return rootView;
     }
 }
