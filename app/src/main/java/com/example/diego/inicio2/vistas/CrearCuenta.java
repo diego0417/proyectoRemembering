38aae0cc17599d377ea07a896be7ec13cf36306a
maximorero14 <maximorero14@gmail.com>
2015-08-20 15:57:15 -0300

    Crear cuenta y loggin

---
+++ app/src/main/java/com/example/diego/inicio2/vistas/CrearCuenta.java
@@ -0,0 +1,159 @@
+package com.example.diego.inicio2.vistas;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.diego.inicio2.Manejadores.ManejadorUsuario;
+import com.example.diego.inicio2.R;
+
+import java.util.Calendar;
+
+public class CrearCuenta extends Activity {
+
+    /* DATE PICKER*/
+    Button btn;
+    int ano_x,mes_x,dia_x;
+    static final int DIALOG_ID=0;
+    TextView fechaNac;
+    TextView linkLoggin;
+    /*-------*/
+    EditText etNombre;
+    EditText etApelldo;
+    RadioButton rtSexo;
+    EditText etMail;
+    EditText etPass;
+    Button btnRegistrarse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+        /* DATE PICKER*/
+
+        final Calendar cal= Calendar.getInstance();
+        ano_x= cal.get(Calendar.YEAR);
+        mes_x = cal.get(Calendar.MONTH);
+        dia_x= cal.get(Calendar.DAY_OF_MONTH);
+        fechaNac = (TextView) findViewById(R.id.txtFechaNaci_Reg);
+        fechaNac.setEnabled(false);
+        showDialogOnButtonClick();
+        LocationToLoggin();
+        /*-------*/
+        btnRegistrarse = (Button) findViewById(R.id.btnRegister_Reg);
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etNombre = (EditText) findViewById(R.id.txtNombre_Reg);
+                etApelldo = (EditText) findViewById(R.id.txtApellido_Reg);
+                etMail = (EditText) findViewById(R.id.txtEmail_Reg);
+                etPass = (EditText) findViewById(R.id.txtPass_Reg);
+                rtSexo = (RadioButton)findViewById(R.id.radio_mascul_Reg);
+                Boolean x = ManejadorUsuario.insertarUsuario(etNombre.getText().toString(), etApelldo.getText().toString(), etMail.getText().toString(), etPass.getText().toString(), rtSexo.isChecked(), fechaNac.getText().toString());
+                if(x)
+                {
+
+                    Toast.makeText(CrearCuenta.this, "Gracias por crear una cuenta En MemoryView", Toast.LENGTH_LONG).show();
+                    Intent inten = new Intent(CrearCuenta.this,Loggin.class);
+                    startActivity(inten);
+
+                }else
+                {
+                    Toast.makeText(CrearCuenta.this,"Algo salio mal",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+    /* ------------------------------------------DATE PICKER-----------------------------------*/
+    public void showDialogOnButtonClick()
+    {
+        btn=(Button) findViewById(R.id.btnMuestra_Reg);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
+            }
+        });
+    }
+
+    public void LocationToLoggin()
+    {
+        linkLoggin= (TextView) findViewById(R.id.linkALoggin_Reg);
+        linkLoggin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inten = new Intent(CrearCuenta.this,Loggin.class);
+                startActivity(inten);
+            }
+        });
+    }
+
+
+
+
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+        if(id==DIALOG_ID)
+        {
+            return new DatePickerDialog(this,dpickerListner,ano_x,mes_x,dia_x);
+        }else
+        {
+            return null;
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner= new DatePickerDialog.OnDateSetListener()
+    {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth)
+        {
+            ano_x=year;
+            mes_x=monthOfYear;
+            dia_x=dayOfMonth;
+            fechaNac.setText(ano_x + "/" + mes_x + "/" + dia_x);
+
+        }
+    };
+
+    /* --------------------------------------------------------------------------------------------------------*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/diego/inicio2/vistas/Loggin.java
@@ -0,0 +1,65 @@
+package com.example.diego.inicio2.vistas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.diego.inicio2.Entidades.Comunicador;
+import com.example.diego.inicio2.Entidades.Usuario;
+import com.example.diego.inicio2.Manejadores.ManejadorUsuario;
+import com.example.diego.inicio2.R;
+
+public class Loggin extends Activity {
+
+    TextView link;
+    Button btnIniciar;
+    EditText etMail;
+    EditText etPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loggin);
+
+
+        link = (TextView) findViewById(R.id.linkAReg_Log);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Loggin.this, CrearCuenta.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnIniciar = (Button) findViewById(R.id.btnEntrar_Log);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressBar pro = (ProgressBar) findViewById(R.id.progressBar_Log);
+                pro.setVisibility(View.VISIBLE);
+                etMail = (EditText) findViewById(R.id.mail);
+                etPass = (EditText) findViewById(R.id.pass);
+                Usuario usuario = ManejadorUsuario.login(etMail.getText().toString(), etPass.getText().toString());
+                if (usuario == null) {
+                    Toast.makeText(Loggin.this, "El mail y/o pass son invalidos", Toast.LENGTH_LONG).show();
+                } else {
+                    //Intent i = new Intent(Loggin.this,Home.class);
+                    //Comunicador.setObjeto(usuario);
+                    //startActivity(i);
+                }
+
+            }});
+
+    }
+}
