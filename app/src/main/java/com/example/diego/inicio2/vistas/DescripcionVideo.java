874332307ebe1d1b93492b1337a12eab623d284f
smatiassosa <sosa.sergiomatias@gmail.com>
2015-08-31 23:32:44 -0300

    Describe video arreglos

--- app/src/main/java/com/example/diego/inicio2/vistas/DescripcionVideo.java
+++ app/src/main/java/com/example/diego/inicio2/vistas/DescripcionVideo.java
@@ -1,26 +1,112 @@
 package com.example.diego.inicio2.vistas;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.diego.inicio2.Entidades.Permiso;
+import com.example.diego.inicio2.Manejadores.ManejadorPermiso;
 import com.example.diego.inicio2.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class DescripcionVideo extends Activity {
 
-    Button listo;
-    EditText titulo;
-    EditText descripcion;
+    Button listo,selecFecha;
+    EditText titulo,descripcion,fecha_desb;
+
+
+    Spinner spinner;
+
+    int ano_x,mes_x,dia_x;
+    static final int DIALOG_ID=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_descripcion_video);
+
+        final Calendar cal= Calendar.getInstance();
+        ano_x= cal.get(Calendar.YEAR);
+        mes_x = cal.get(Calendar.MONTH);
+        dia_x= cal.get(Calendar.DAY_OF_MONTH);
+        fecha_desb = (EditText) findViewById(R.id.txtFechaDesbloqueo_DescVideo);
+        fecha_desb.setEnabled(false);
+        showDialogOnButtonClick();
+
+
+
+        spinner=(Spinner) findViewById(R.id.spinnerVisibilidad_DescVideo);
+        String[] permisos= {};
+
+        ArrayList<Permiso> per = ManejadorPermiso.getAll();
+        Log.e("Entre","Entre");
+        int i=0;
+        for(Permiso x: per)
+        {
+            Log.e("SEEEEEE","SEEEEEE");
+
+            Log.e("Valor 1","trola  "+x.getDescripcionCorta().toString());
+            permisos[i]=x.getDescripcionCorta().toString();
+            i++;
+
+        }
+        Log.e("Sali","Sali");
+        ArrayAdapter<String> permi = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,permisos);
+
+
     }
+
+    public void showDialogOnButtonClick()
+    {
+        selecFecha=(Button) findViewById(R.id.btnFecha_DescVideo);
+
+        selecFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+        if(id==DIALOG_ID)
+        {
+            return new DatePickerDialog(this,dpickerListner,ano_x,mes_x,dia_x);
+        }else
+        {
+            return null;
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner= new DatePickerDialog.OnDateSetListener()
+    {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth)
+        {
+            ano_x=year;
+            mes_x=monthOfYear;
+            dia_x=dayOfMonth;
+            fecha_desb.setText(ano_x + "/" + mes_x + "/" + dia_x);
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
